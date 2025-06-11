dec5dcf397a8ca1db33a72d76d4c592cde0d3883
Dulce Nevarez <dulcenev04@gmail.com>
2025-06-11 12:37:07 -0500

    Añadir el servicio Giphy e interfaces; actualizar la configuracion de los enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,4 +4,7 @@ export const environment = {
     companyName: 'Gifs',
     companyName2: 'App',
     companySlogan: 'Maneja tus gifs',
+
+    giphyApiKey: 'NDZDSTMeCobVRLVFq6mBtzbP3X56V3FP',
+    giphyUrl: 'https://api.giphy.com/v1'
 };
